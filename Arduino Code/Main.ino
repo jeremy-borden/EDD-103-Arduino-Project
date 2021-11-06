1584ecffc6927c87379d5b70877d5657d9aad440
jeremy-borden <jeremygborden@gmail.com>
2021-11-06 13:00:55 -0400

    Update Main.ino

--- Arduino Code/Main.ino
+++ Arduino Code/Main.ino
@@ -1,23 +1,28 @@
-#include <BetterJoystick.h>
 #include <LiquidCrystal.h>
 #include <FastLED.h>
 
 LiquidCrystal lcd = LiquidCrystal(2, 3, 4, 5, 6, 7);
-Joystick joystick(A0, A1, 8); // X, Y, switch
+//possibly make separate class for input stuff? joystick + button
+double joyX; // joystick x coordinate
+double joyY; // joystick y coordinate
 
 void setup()
 {
     lcd.begin(16, 2);
-    Serial.begin();
 }
 
 void loop()
 {
-    lcd.print("X: ");
-    lcd.print(joystick.x());
-    lcd.setCursor(0, 1);
-    lcd.print("Y: ");
-    lcd.print(joystick.y());
-    delay(500);
-    lcd.clear();
+    joyX = map(analogRead(A0), 0, 1023, -512, 512) / 512.0;
+    joyY = -map(analogRead(A1), 0, 1023, -512, 512) / 512.0;
+}
+
+int joystickAngle() //return angle of joystick form 0 to 359 degrees
+{
+    return (int)(degrees(atan2(-joyY, -joyX)) + 180) % 360;
+}
+
+double joystickNormalized() // return normalized magnitude of joystick
+{
+    return sqrt(sq(joyY * sqrt(1.0 - (joyX * joyX * .5))) + sq(joyX * sqrt(1.0 - (joyY * joyY * .5))));
 }
