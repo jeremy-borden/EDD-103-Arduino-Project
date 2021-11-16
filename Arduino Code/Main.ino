dd7914446ddb86dad2d1a3b0683c5dda9b174261
jeremy-borden <jeremygborden@gmail.com>
2021-11-16 11:27:49 -0500

    Update Main.ino

--- Arduino Code/Main.ino
+++ Arduino Code/Main.ino
@@ -14,8 +14,17 @@
 #define NUM_LEDS 144
 CRGB leds[NUM_LEDS];
 
-double joystickX;
-double joystickY;
+//input
+double joystickX = 0;
+double joystickY = 0;
+int joystickAngle = 0;
+double joystickMagnitude = 0;
+int joystickDirection = 0;
+
+bool buttonPressed = false;
+bool buttonReleased = true;
+bool joystickMoved = false;
+bool joystickReleased = true;
 
 LiquidCrystal_74HC595 lcd(11, 13, 12, 1, 3, 4, 5, 6, 7);
 
@@ -31,7 +40,16 @@ byte sevenSegDigits[10] = {
     B11111011, // = 8
     B11001011  // = 9
 };
+//Game
+enum class GameState
+{
+    Menu,
+    Game
+};
 
+GameState gameState = GameState::Menu;
+int menuScreen = 0;
+int selectedScreen = NULL;
 boolean isPlaying = false;
 int patternArray[255];
 int numColors = 4;
@@ -49,42 +67,109 @@ void setup()
     pinMode(SSD_LATCH_PIN, OUTPUT);
     pinMode(SSD_CLOCK_PIN, OUTPUT);
     pinMode(SSD_DATA_PIN, OUTPUT);
+
+    lcd.begin(16, 2);
 }
 
 void loop()
 {
-<<<<<<< HEAD
-    for(int i = 0; i<10; i++){
-        displayDigit(i);
-        delay(100);
+    inputUpdate();
+
+    if (gameState == GameState::Menu)
+    {
+        lcd.setCursor(0, 0);
+        lcd.print("<");
+        lcd.setCursor(15, 0);
+        lcd.print(">");
+        switch (menuScreen)
+        {
+        case 0: // Play screen
+            lcd.setCursor(6, 0);
+            lcd.print("Play");
+            if (buttonPressed)
+            {
+                selectedScreen = 0;
+            }
+            break;
+        case 1: // Change color num screen
+            lcd.setCursor(2, 0);
+            lcd.print("# of Colors");
+            lcd.setCursor(7, 1);
+            lcd.print(numColors);
+            if (buttonPressed)
+            {
+                selectedScreen = 1;
+            }
+            break;
+        case 2: // Change time to answer screen
+            lcd.setCursor(2, 0);
+            lcd.print("Answer Time");
+            lcd.setCursor(7, 1);
+            lcd.print(timeToAnswer);
+            if (buttonPressed)
+            {
+                selectedScreen = 2;
+            }
+            break;
+        }
+        //switch to handle sleected screen?
+        switch (selectedScreen)
+        {
+        case NULL: //no screen is selected, allow player to scroll thru screens
+
+            break;
+        }
     }
-=======
->>>>>>> 703dadf66da4f598792dfe0d1b013e8d05b28fad
-    
+
+    lcd.clear();
 }
 
 //INPUT FUNCTIONS
-int joystickAngle()
+void inputUpdate()
 {
-    return (int)(degrees(atan2(-joystickY, -joystickX)) + 180) % 360;
-}
+    buttonPressed = digitalRead(BUTTON_PIN) && buttonReleased;
+    buttonReleased = !digitalRead(BUTTON_PIN);
 
-double joystickMagnitude()
-{
-    return sqrt(sq(joystickY * sqrt(1.0 - (joystickX * joystickX * .5))) + sq(joystickX * sqrt(1.0 - (joystickY * joystickY * .5))));
-}
-
-void joystickUpdate()
-{
     joystickX = map(analogRead(JOYSTICK_X_PIN), 0, 1023, -512, 512) / 512.0;
     joystickY = -map(analogRead(JOYSTICK_Y_PIN), 0, 1023, -512, 512) / 512.0;
+
+    joystickAngle = (int)(degrees(atan2(-joystickY, -joystickX)) + 180) % 360;
+    joystickMagnitude = sqrt(sq(joystickY * sqrt(1.0 - (joystickX * joystickX * .5))) + sq(joystickX * sqrt(1.0 - (joystickY * joystickY * .5))));
+
+    joystickMoved = joystickMagnitude>3;
+    joystickDirection = calculateJoystickDirection();
+}
+
+int calculateJoystickDirection()
+{
+    if (joystickMagnitude < 0.7)
+    {
+        return 0; //CENTER
+    }
+    if (joystickAngle < 45 || joystickAngle > 315)
+    {
+        return 1; //RIGHT
+    }
+    if (joystickAngle > 45 && joystickAngle < 135)
+    {
+        return 2; //UP
+    }
+    if (joystickAngle > 135 && joystickAngle < 225)
+    {
+        return 3; //LEFT
+    }
+    if (joystickAngle > 225 && joystickAngle < 315)
+    {
+        return 4; //DOWN
+    }
+    return 0;
 }
 
 //DISPLAY FUNCTIONS
-void displayDigit(int digit) // display digit on 7sd, -1 to clear display
+void displayDigit(int digit) // display digit on 7sd, NULL to clear display
 {
     digitalWrite(SSD_LATCH_PIN, LOW);
-    if (digit == -1)
+    if (digit == NULL)
     {
         shiftOut(SSD_DATA_PIN, SSD_CLOCK_PIN, MSBFIRST, B00000000);
     }
@@ -94,5 +179,3 @@ void displayDigit(int digit) // display digit on 7sd, -1 to clear display
     }
     digitalWrite(SSD_LATCH_PIN, HIGH);
 }
-
-//MENU FUNCTIONS
