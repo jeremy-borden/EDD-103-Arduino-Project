f70b476c4923d74d9c8a3300f0f7af9796cc17f9
jeremy-borden <jeremygborden@gmail.com>
2021-12-01 03:18:29 -0500

    Update Main.ino

--- Arduino Code/Main.ino
+++ Arduino Code/Main.ino
@@ -22,8 +22,8 @@ CRGBArray<INTERNAL_NUM_LEDS + EXTERNAL_NUM_LEDS> leds;
 float joystickX = 0;
 float joystickY = 0;
 float joystickMagnitude = 0;
-int joystickAngle = 0; // change to uint_t
-int8_t joystickDirection = 0;
+int joystickAngle = 0;
+int joystickDirection = 0;
 
 bool buttonPressed = false;
 bool buttonReleased = true;
@@ -34,15 +34,15 @@ LiquidCrystal_74HC595 lcd(11, 13, 12, 1, 3, 4, 5, 6, 7);
 
 const byte sevenSegDigits[10] = {
     B01111011, // = 0
-    B00001001, // = 1
-    B10110011, // = 2
-    B10011011, // = 3
-    B11001001, // = 4
-    B11011010, // = 5
-    B11111000, // = 6
-    B00001011, // = 7
-    B11111011, // = 8
-    B11001011  // = 9
+    B01100000, // = 1
+    B00110111, // = 2
+    B01110110, // = 3
+    B01101100, // = 4
+    B01011110, // = 5
+    B01011111, // = 6
+    B01110000, // = 7
+    B01111111, // = 8
+    B01111100  // = 9
 };
 
 //Game
@@ -65,14 +65,14 @@ int8_t roundNum = 1;
 int8_t timeLeft = 0;
 int8_t inputNum = 0;
 
-const CRGB colorList[] = {
+CRGB colorList[] = {
     CRGB::Blue, CRGB::Red, CRGB::Green, CRGB::Yellow,                     //
     CRGB::DarkOrchid, CRGB::DarkOrange, CRGB::Fuchsia, CRGB::GreenYellow, //
-    CRGB::Cyan, CRGB::Maroon, CRGB::Teal, CRGB::MidnightBlue};
+    CRGB::Cyan, CRGB::Maroon, CRGB::Teal, CRGB::Chartreuse};
 
 bool colorActiveList[12]; //true if color should be bright
 
-Timer<3, millis, uint8_t> timerColor;
+Timer<16, millis, uint8_t> timerColor;
 
 #define MAX_BUZZER_FREQ 2000
 #define MIN_BUZZER_FREQ 1000
@@ -110,43 +110,30 @@ void setup()
     pinMode(BUTTON_PIN, INPUT_PULLUP);
 
     randomSeed(analogRead(A5));
-    //timer.in(0, resetGame);
-    menuScreen = 0;
-    selectedScreen = -1;
-    roundNum = 1;
-    timeLeft = 0;
-    inputNum = 0;
-    isPlayerTurn = false;
-    displayDigit(-1);
-    timerColor.empty();
-    timer.empty();
-    gameState = GameState::MENU;
-    tone(BUZZER_PIN, 900, 50);
+    resetGame();
 }
 
 void loop()
 {
-
     inputUpdate();
-    
-
 
     if (gameState == GameState::MENU)
     {
         menu();
     }
-    if (gameState == GameState::GAME)
+    else if (gameState == GameState::GAME)
     {
         game();
     }
-    if (gameState == GameState::LOSE)
+    else if (gameState == GameState::LOSE)
     {
         lose();
     }
 
     timer.tick();
+    timerColor.tick();
     displayLEDs();
-    //FastLED.show();
+
     EVERY_N_MILLIS(50)
     {
         lcd.clear();
@@ -159,10 +146,10 @@ void inputUpdate()
     buttonPressed = !digitalRead(BUTTON_PIN) && buttonReleased;
     buttonReleased = digitalRead(BUTTON_PIN);
 
-    joystickX = map(analogRead(JOYSTICK_X_PIN), 0, 1023, -512, 512) / 512.0f;
-    joystickY = -map(analogRead(JOYSTICK_Y_PIN), 0, 1023, -512, 512) / 512.0f;
+    joystickX = map(analogRead(JOYSTICK_X_PIN), 0, 1023, -512, 512) / 512.0;
+    joystickY = -map(analogRead(JOYSTICK_Y_PIN), 0, 1023, -512, 512) / 512.0;
 
-    joystickAngle = (int)(degrees(atan2(-joystickY, -joystickX)) + 180) % 360;
+    joystickAngle = (int)(degrees(atan2(-joystickY, joystickX)) + 180) % 360;
     joystickMagnitude = sqrt(sq(joystickY * sqrt(1.0 - (joystickX * joystickX * .5))) + sq(joystickX * sqrt(1.0 - (joystickY * joystickY * .5))));
 
     joystickMoved = joystickMagnitude >= 0.5 && joystickReleased;
@@ -171,7 +158,7 @@ void inputUpdate()
     joystickDirection = getJoystickDirection();
 }
 
-int8_t getJoystickDirection()
+int getJoystickDirection()
 {
     if (joystickMagnitude < 0.5)
     {
@@ -217,36 +204,36 @@ void displayDigit(int digit) // display digit on 7sd, -1 to clear display
 //PROCESSES
 void menu()
 {
-    if (selectedScreen == -1)
-    {
+    // if (selectedScreen == -1)
+    // {
 
-        switch (menuScreen)
+    switch (menuScreen)
+    {
+    case 0: // Play screen
+        lcd.setCursor(6, 0);
+        lcd.print(F("Play"));
+        break;
+    case 1: // Change color num screen
+        lcd.setCursor(2, 0);
+        lcd.print(F("# of Colors"));
+        lcd.setCursor(7, 1);
+        lcd.print(numColors);
+        break;
+    case 2: // Change time to answer screen
+        lcd.setCursor(2, 0);
+        lcd.print(F("Answer Time"));
+        lcd.setCursor(7, 1);
+        if (timeToAnswer == 10)
         {
-        case 0: // Play screen
-            lcd.setCursor(6, 0);
-            lcd.print(F("Play"));
-            break;
-        case 1: // Change color num screen
-            lcd.setCursor(2, 0);
-            lcd.print(F("# of Colors"));
-            lcd.setCursor(7, 1);
-            lcd.print(numColors);
-            break;
-        case 2: // Change time to answer screen
-            lcd.setCursor(2, 0);
-            lcd.print(F("Answer Time"));
-            lcd.setCursor(7, 1);
-            if (timeToAnswer == 10)
-            {
-                lcd.print(F("INF"));
-            }
-            else
-            {
-                lcd.print(timeToAnswer);
-            }
-            break;
+            lcd.print(F("INF"));
         }
+        else
+        {
+            lcd.print(timeToAnswer);
+        }
+        break;
     }
+
     if (buttonPressed && selectedScreen == -1)
     {
         selectedScreen = menuScreen;
@@ -370,6 +357,10 @@ void menu()
 
 void game() // use isPlayerTurn to choose whether a pattern is showing or player is supposed to input
 {
+    lcd.setCursor(3,0);
+    lcd.print(F("Round"));
+    lcd.setCursor(10,0);
+    lcd.print(roundNum);
     if (!isPlayerTurn)
     {
 
@@ -387,6 +378,7 @@ void game() // use isPlayerTurn to choose whether a pattern is showing or player
                 return;
             }
             colorActiveList[patternList[inputNum]] = true;
+            timerColor.in(250, disableSection, patternList[inputNum]);
             //play sound
             inputNum++;
         }
@@ -398,6 +390,7 @@ void game() // use isPlayerTurn to choose whether a pattern is showing or player
         {
             EVERY_N_SECONDS(1)
             {
+                displayDigit(timeLeft);
                 timeLeft--;
                 if (timeLeft < 0)
                 {
@@ -426,7 +419,7 @@ void game() // use isPlayerTurn to choose whether a pattern is showing or player
                 inputNum++;
                 timeLeft = timeToAnswer;
                 colorActiveList[input] = true;
-                timerColor.in(200, disableSection, input);
+                timerColor.in(100, disableSection, input);
             }
             else
             {
@@ -439,7 +432,18 @@ void game() // use isPlayerTurn to choose whether a pattern is showing or player
         {
             roundNum++;
             inputNum = 0;
+     
+            long now = millis();
+            while (millis() - now < 200)
+            {
+                timerColor.tick();
+            }
+            for (int i = 0; i < 12; i++)
+            {
+                colorActiveList[i] = false;
+            }
             isPlayerTurn = false;
+
         }
     }
 }
@@ -467,41 +471,44 @@ void displayLEDs() // use this to handle what the led strip should be doing
     //display outer ring of colors
     for (int i = 0; i < numColors; i++)
     { //FIX BOUNDS LATER
-        int startLed = i / ((double)numColors) * EXTERNAL_NUM_LEDS;
+        int startLed = (numColors - (i+1)) / ((double)numColors) * EXTERNAL_NUM_LEDS;
         int numLedsInSection = EXTERNAL_NUM_LEDS / numColors;
         int endLed = startLed + numLedsInSection - 1;
         fill_solid(leds(INTERNAL_NUM_LEDS + startLed, INTERNAL_NUM_LEDS + endLed), numLedsInSection, colorList[i]);
         if (!colorActiveList[i])
         { // if the color is not "active" dim it a bit
-            leds(INTERNAL_NUM_LEDS + startLed, INTERNAL_NUM_LEDS + endLed).fadeLightBy(210);
+            leds(INTERNAL_NUM_LEDS + startLed, INTERNAL_NUM_LEDS + endLed).fadeLightBy(250);
         }
     }
 
     //inner ring selection
     if (joystickMagnitude > JOYSTICK_DEADZONE)
     {
-        int x = map(joystickAngle, 0, 359, 0, INTERNAL_NUM_LEDS - 1);
-        CRGB colorPointedAt = colorList[getJoystickColorIndex()];
-        //fade color by joystick magnitude?
-        leds[((x + 2) % INTERNAL_NUM_LEDS)] = colorPointedAt;
-        leds[((x + 1) % INTERNAL_NUM_LEDS)] = colorPointedAt;
-        leds[x] = colorPointedAt;
-        leds[((x - 1) % INTERNAL_NUM_LEDS)] = colorPointedAt;
-        leds[((x - 2) % INTERNAL_NUM_LEDS)] = colorPointedAt;
+        int adjustedAngle = (((-1 * joystickAngle) + 90) % 360) + 270;
+        adjustedAngle = (adjustedAngle+140) % 360;
+        int x = map(adjustedAngle, 0, 359, 1, (INTERNAL_NUM_LEDS - 1));
+        Serial.println(adjustedAngle);
+        CRGB colorPointedAt = colorList[getJoystickColorIndex(adjustedAngle)];
 
-        leds[((x + 2) % INTERNAL_NUM_LEDS)].fadeLightBy(220);
-        leds[((x + 1) % INTERNAL_NUM_LEDS)].fadeLightBy(192);
-        leds[((x - 1) % INTERNAL_NUM_LEDS)].fadeLightBy(192);
-        leds[((x - 2) % INTERNAL_NUM_LEDS)].fadeLightBy(220);
+        // leds[((x + 2) % INTERNAL_NUM_LEDS)] = colorPointedAt;
+        // leds[((x + 1) % INTERNAL_NUM_LEDS)] = colorPointedAt;
+        leds[x] = colorPointedAt;
+        // leds[((x - 1) % INTERNAL_NUM_LEDS)] = colorPointedAt;
+        // leds[((x - 2) % INTERNAL_NUM_LEDS)] = colorPointedAt;
+
+        // leds[((x + 2) % INTERNAL_NUM_LEDS)].fadeLightBy(220);
+        // leds[((x + 1) % INTERNAL_NUM_LEDS)].fadeLightBy(192);
+        // leds[((x - 1) % INTERNAL_NUM_LEDS)].fadeLightBy(192);
+        // leds[((x - 2) % INTERNAL_NUM_LEDS)].fadeLightBy(220);
     }
     leds(0, INTERNAL_NUM_LEDS - 1).fadeToBlackBy(50);
 
-    
+    FastLED.show();
 }
 
-int getJoystickColorIndex() //get color joystick is pointing to;
+int getJoystickColorIndex(int angle) //get color joystick is pointing to
 {
-    return (int)(joystickAngle * numColors / 360.0f);
+    return (int)(angle * numColors / 360.0f);
 }
 
 int getInput()
@@ -509,7 +516,9 @@ int getInput()
 
     if (joystickMagnitude > JOYSTICK_DEADZONE && buttonPressed)
     {
-        return getJoystickColorIndex();
+        int adjustedAngle = (((-1 * joystickAngle) + 90) % 360) + 270;
+        adjustedAngle = (adjustedAngle+140) % 360;
+        return getJoystickColorIndex(adjustedAngle);
     }
     else
     {
